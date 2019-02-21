144290b386ad0954001ffe651d258472f1f600f8
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-21 03:57:33 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,10 +2,15 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
-  collection1 = []
-  while  i < collection.length
-    collection1 << yield(collection[i])
-    i += 1
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
   end
-  
-  if
+ 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+end
